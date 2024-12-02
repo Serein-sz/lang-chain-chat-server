a5399b26fa47b69dba71147af15d6d2a07986d68
王强 <wangqiang@hfvast.com>
2024-12-02 22:06:58 +0800

    <feat>: migrate ollama model to ali server

--- util/ollama.go
+++ util/ollama.go
@@ -7,8 +7,7 @@ import (
 )
 
 func CreateOllama(c *gin.Context, modelName string) *ollama.LLM {
-	llm, err := ollama.New(ollama.WithModel(modelName))
-
+	llm, err := ollama.New(ollama.WithModel(modelName), ollama.WithServerURL("http://112.125.89.224:11434"))
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": err.Error(),
